095b3219c1b98cabf20f1bae39533dd29a98eb91
Salah-Zain <salahzain36@gmail.com>
2024-11-09 15:10:37 +0530

    service page add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import styled from 'styled-components'
 import Home from './components/home'
 import About from './components/about'
+import Service from './service/service'
 function App() {
 
 
@@ -9,7 +10,7 @@ function App() {
     <>
 <Home/>  
 <About/>   
-    
+    <Service/>
       
     </>
   )
